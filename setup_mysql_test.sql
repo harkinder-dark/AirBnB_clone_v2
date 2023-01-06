d2a7275fc4e42ffbee88638a84230a0b47624533
harkinder-dark <victorvaddely@gmail.com>
2023-01-06 16:43:23 +0000

    deadline day

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,9 +1,9 @@
---Write a script that prepares a MySQL server for the project
+-- Write a script that prepares a MySQL server for the project
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 -- create user
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost'
     IDENTIFIED BY 'hbnb_test_pwd';
--- permission for hbnb_dev_db
+-- permission for hbnb_test_db
 GRANT ALL
     ON `hbnb_test_db`.*
     TO 'hbnb_test'@'localhost';
