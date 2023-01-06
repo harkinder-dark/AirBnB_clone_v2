e1598b3629199891c5e7ef6ed8b7fd88ec35acf6
harkinder-dark <victorvaddely@gmail.com>
2023-01-06 19:49:03 +0000

    deadline day

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,17 +2,19 @@
 """This module defines a class to manage file storage for hbnb clone"""
 import json
 import models
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
     __file_path = 'file.json'
     __objects = {}
-
-    def delete(self, obj=None):
-        """deleting objects"""
-        if obj:
-            self.__objects.pop("{}.{}".format(type(obj).__name__, obj.id))
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
@@ -40,14 +42,6 @@ class FileStorage:
 
     def reload(self):
         """Loads storage dictionary from file"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
-
         classes = {
                     'BaseModel': BaseModel, 'User': User, 'Place': Place,
                     'State': State, 'City': City, 'Amenity': Amenity,
@@ -61,3 +55,13 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+    
+    def delete(self, obj=None):
+        """deleting objects"""
+        if obj:
+            self.__objects.pop("{}.{}".format(type(obj).__name__, obj.id))
+    
+    def close(self):
+        """display our HBNB data
+        """
+        self.reload()
