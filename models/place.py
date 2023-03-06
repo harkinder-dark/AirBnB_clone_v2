215390d2f10b50e0d8edb63b6911674edd36c8a3
harkinder-dark <victorvaddely@gmail.com>
2023-03-06 14:15:59 +0000

    after

--- models/place.py
+++ models/place.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy import Integer, Float, Table
 from os import environ
