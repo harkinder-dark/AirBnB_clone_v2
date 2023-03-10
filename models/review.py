796b944a2eb0ad70594f3159f8a828105a7e5b2f
harkinder-dark <victorvaddely@gmail.com>
2023-03-10 14:48:55 +0000

    deadline

--- models/review.py
+++ models/review.py
@@ -14,8 +14,8 @@ class Review(BaseModel):
     if (storage_engine == 'db'):
         __tablename__ = "reviews"
         text = Column(String(1024), nullable=False)
-        place_id = Column(String(60), nullable=False, ForeignKey("places.id"))
-        user_id = Column(String(60), nullable=False, ForeignKey("users.id"))
+        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     else:
         place_id = ""
         user_id = ""
